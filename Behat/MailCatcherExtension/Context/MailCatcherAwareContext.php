32a6cb74f47325193a50e7a9b9802ab4a1ad0c6d
David Delevoye <david.delevoye@oxylaneonline.com>
2014-10-20 20:52:55 +0200

    rename interface

---
+++ Behat/MailCatcherExtension/Context/MailCatcherAwareContext.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Alex\MailCatcher\Behat\MailCatcherExtension\Context;
+
+use Behat\Behat\Context\Context;
+use Alex\MailCatcher\Client;
+
+/**
+ * @author Alexandre Salomé <alexandre.salome@gmail.com>
+ * @author David Delevoye <daviddelevoye@gmail.com>
+ */
+interface MailCatcherAwareContext extends Context
+{
+    public function setConfiguration(Client $mailcatcher, $purgeBeforeScenario = true);
+}
